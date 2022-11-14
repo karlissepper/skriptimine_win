d2b4911e67157e61ad180dfd3199af6017c0e5d6
Karlis Sepper <karlissepper@voco.ee>
2022-11-14 10:56:20 +0200

    test

---
+++ kasutaja_loomine_2.ps1
@@ -0,0 +1,14 @@
+﻿#  defineerime vajalikud käsurea parameetrid
+$Fail = "C:\Users\Administrator\Documents\skriptimine\kasutajad.csv"
+#Loeme failist sisu, info jagatakse ; järgi
+$Kasutajad = Import-Csv $Fail -Encoding Default -Delimiter ";"
+foreach ($kasutaja in $kasutajad)
+{
+#defineerimie muutujad argumentide salvestamiseks
+$Kasutajanimi = $kasutaja.Kasutajanimi
+$TaisNimi = $kasutaja.Taisnimi
+$KonotKirjeldus = $kasutaja.KontoKirjeldus
+$KasutajaParool = $kasutaja.Parool | ConvertTo-SecureString -AsPlainText -Force
+#lisame kasutaja vastavate andmetega
+New-LocalUser "$Kasutajanimi" -Password $KasutajaParool -FullName "$TaisNimi" -Description $KonotKirjeldus
+}
